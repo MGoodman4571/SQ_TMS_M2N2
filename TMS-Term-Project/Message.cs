1b293355dc5f6c19e2b3d8bb86e6edf5a6ec9e0b
michaelwoan97 <nghia00720@yahoo.com.vn>
2020-10-23 21:29:02 -0400

    day 0 repository setup

---
+++ TMS-Term-Project/Message.cs
@@ -0,0 +1,19 @@
+﻿/**
+ *      FILE            :       Message.cs
+ *      PROJECT         :       
+ *      PROGRAMMER      :      
+ *      DESCRIPTION     :       
+ *      FIRST VERSION   :       2020-10-17
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_Term_Project
+{
+    class Message
+    {
+    }
+}
